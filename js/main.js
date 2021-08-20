0ac86dbe88bb52d07efe9942f424b9b1250518bf
Rafael Almendra <rafaelalmendra28@gmail.com>
2021-08-19 23:44:17 -0300

    contact and scroll smooth

--- js/main.js
+++ js/main.js
@@ -38,3 +38,21 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 });
+
+/* ScrollReveal show elements to page*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+});
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonails,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+  )
