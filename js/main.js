2abae67137478ce12d4c9df7c5949c68deeddfa0
Rafael Almendra <rafaelalmendra28@gmail.com>
2021-08-24 23:51:11 -0300

    finalização

--- js/main.js
+++ js/main.js
@@ -18,10 +18,10 @@ for(const link of links) {
 }
 
 /* change the page header when scrolling */
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
   if(window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -66,24 +66,42 @@ scrollReveal.reveal(
   )
 
 /* back up button */
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
-
   if(this.window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
-
+}
 /* menu active */
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll('main section[id]')
 function activateMenuAtCurrentSection() {
-  
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for ( const section of sections ) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
 }
 
-
 /* when scroll */
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
